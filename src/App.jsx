a052d2bf419731bfecb902adbb39c61016508f75
SouzaMVictor <souzamvictor@gmail.com>
2025-02-25 17:18:31 -0300

    add CityItem component and enhance CityList with loading state and city data fetching

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,26 @@ import PageNotFound from "./pages/PageNotFound";
 import AppLayout from "./pages/AppLayout";
 import Login from "./pages/Login";
 import CityList from "./Components/CityList";
-
+import { useEffect, useState } from "react";
+const BASE_URL = "http://localhost:9000/";
 function App() {
+  const [cities, setCities] = useState([]);
+  const [isloading, setIsLoading] = useState(false);
+  useEffect(function () {
+    async function fetchCities() {
+      try {
+        setIsLoading(true);
+        const res = await fetch(`${BASE_URL}cities`);
+        const data = await res.json();
+        setCities(data);
+      } catch {
+        alert("there was an error loading data...");
+      } finally {
+        setIsLoading(false);
+      }
+    }
+    fetchCities();
+  }, []);
   //introdução a react router: diferentes url pra diferentes componentes --> ROUTES
   //essencial para SINGLE PAGE APLICATIONS
   //pagina nunca é recarregada
@@ -20,8 +38,8 @@ function App() {
         <Route path="pricing" element={<Pricing />} />
         <Route path="*" element={<PageNotFound />} />
         <Route path="app" element={<AppLayout />}>
-          <Route index element={<CityList />} />
-          <Route path="cities" element={<CityList />} />
+          <Route index element={<CityList isloading={isloading} />} />
+          <Route path="cities" element={<CityList cities={cities} />} />
           <Route path="countries" element={<p>Countries</p>} />
           <Route path="form" element={<p>Form</p>} />
         </Route>

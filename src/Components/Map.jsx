81d4d2cd1046d90142f326eb758ee40c924fba5c
SouzaMVictor <souzamvictor@gmail.com>
2025-02-26 00:11:29 -0300

    add Button component and integrate it into Form; enhance Map to navigate on click. programmatic navigation with useNavigate

--- src/Components/Map.jsx
+++ src/Components/Map.jsx
@@ -1,12 +1,18 @@
 import styles from "./Map.module.css";
-import { useSearchParams } from "react-router-dom";
+import { useNavigate, useSearchParams } from "react-router-dom";
 function Map() {
+  const navigate = useNavigate();
   const [searchParams, setSearchParams] = useSearchParams();
 
   const lat = searchParams.get("lat");
   const lng = searchParams.get("lng");
   return (
-    <div className={styles.mapContainer}>
+    <div
+      className={styles.mapContainer}
+      onClick={() => {
+        navigate("form");
+      }}
+    >
       <h1>map</h1>
       <h1>
         position: {lat}, {lng}

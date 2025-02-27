8cdc87d46397f3b4b842270c9f3b088680df7126
SouzaMVictor <souzamvictor@gmail.com>
2025-02-27 04:24:57 -0300

    add BackButton component and integrate it into City and Form components for improved navigation

--- src/Components/CityItem.jsx
+++ src/Components/CityItem.jsx
@@ -1,5 +1,6 @@
 import { Link } from "react-router-dom";
 import styles from "./CityItem.module.css";
+import { useCities } from "../contexts/CitiesContext";
 const flagemojiToPNG = (flag) => {
   var countryCode = Array.from(flag, (codeUnit) => codeUnit.codePointAt())
     .map((char) => String.fromCharCode(char - 127397).toLowerCase())
@@ -8,19 +9,24 @@ const flagemojiToPNG = (flag) => {
     <img src={`https://flagcdn.com/24x18/${countryCode}.png`} alt="flag" />
   );
 };
+
 const formatDate = (date) =>
   new Intl.DateTimeFormat("en", {
     day: "numeric",
     month: "long",
     year: "numeric",
   }).format(new Date(date));
+
 function CityItem({ city }) {
+  const { currentCity } = useCities();
   const { cityName, emoji, date, id, position } = city;
-  console.log(position);
+
   return (
     <li>
       <Link
-        className={styles.cityItem}
+        className={`${styles.cityItem} ${
+          id === currentCity.id ? styles["cityItem--active"] : ""
+        }`}
         to={`${id}?lat=${position.lat}&lng=${position.lng}`}
       >
         <span className={styles.emoji}>{flagemojiToPNG(emoji)}</span>

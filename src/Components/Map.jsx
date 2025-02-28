3c2fcc515116a97dc7bf288c82606c0934e032f6
SouzaMVictor <souzamvictor@gmail.com>
2025-02-28 02:30:05 -0300

    refactor(Map): update geolocation handling to set map position on geolocation change

--- src/Components/Map.jsx
+++ src/Components/Map.jsx
@@ -43,11 +43,20 @@ function Map() {
     [mapLat, mapLng]
   );
 
+  useEffect(
+    function () {
+      if (geolocationPosition)
+        setMapPosition([geolocationPosition.lat, geolocationPosition.lng]);
+    },
+    [geolocationPosition]
+  );
   return (
     <div className={styles.mapContainer}>
-      <Button type="position" onClick={getPosition}>
-        {isLoadingPosition ? "Loading..." : "Use your position"}
-      </Button>
+      {!geolocationPosition && (
+        <Button type="position" onClick={getPosition}>
+          {isLoadingPosition ? "Loading..." : "Use your position"}
+        </Button>
+      )}
       <MapContainer
         center={mapPosition}
         zoom={7}

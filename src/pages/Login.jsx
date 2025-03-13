4d7710b1d4bc1247e78cd44a3a2b457a9382d30d
SouzaMVictor <souzamvictor@gmail.com>
2025-03-13 02:32:47 -0300

    changed the hard coded button for the button component

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,16 +1,21 @@
 import { useState } from "react";
 import styles from "./Login.module.css";
 import PageNav from "../Components/PageNav";
+import Button from "../Components/Button";
 
 export default function Login() {
   // PRE-FILL FOR DEV PURPOSES
   const [email, setEmail] = useState("jack@example.com");
   const [password, setPassword] = useState("qwerty");
 
+  function handleSubmit(e) {
+    e.preventDefault();
+  }
+
   return (
     <main className={styles.login}>
       <PageNav />
-      <form className={styles.form}>
+      <form className={styles.form} onSubmit={handleSubmit}>
         <div className={styles.row}>
           <label htmlFor="email">Email address</label>
           <input
@@ -32,7 +37,7 @@ export default function Login() {
         </div>
 
         <div>
-          <button>Login</button>
+          <Button type="primary">Login</Button>
         </div>
       </form>
     </main>
